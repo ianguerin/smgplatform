d76450fe5b2dfccc59691d15e92fdbabb4bc4731
Ian Guerin <img236@nyu.edu>
2014-12-08 16:53:17 -0500

    push

--- phonegapCode.js
+++ phonegapCode.js
@@ -18,21 +18,20 @@ function fbLoginSuccess(userData) {
 function successHandler (result) {
   alert('result = ' + result);
 }
-
 function errorHandler (error) {
   alert('error = ' + error);
 }
 
 function onDeviceReady() {
-  alert("onDeviceReady");
 
-  // facebookConnectPlugin.login(["public_profile"],
-  //   fbLoginSuccess,
-  //   function (error) { sendToken("", error); }
-  // );
+// facebookConnectPlugin.login(["public_profile"],
+//   fbLoginSuccess,
+//   function (error) { sendToken("", error); }
+// );
 
+  alert('onDeviceReady4');
   var pushNotification = window.plugins.pushNotification;
-  if ( cordova.platformId == "android" || cordova.platformId == "Android" || cordova.platformId == "amazon-fireos" ){
+  if ( cordova.platformId == 'android' || cordova.platformId == 'Android' || cordova.platformId == "amazon-fireos" ){
     pushNotification.register(
       successHandler,
       errorHandler,
@@ -52,9 +51,10 @@ function onDeviceReady() {
       });
   }
 }
+
 // iOS
 function onNotificationAPN(event) {
-  alert("RECEIVED:" + JSON.stringify(event));
+  alert('RECEIVED:' + JSON.stringify(event));
   if ( event.alert )
   {
       navigator.notification.alert(event.alert);
@@ -75,21 +75,21 @@ function onNotificationAPN(event) {
 function tokenHandler(result) {
   // Your iOS push server needs to know the token before it can push to this device
   // here is where you might want to send it the token for later use.
-  alert("device token = " + result);
+  alert('device token = ' + result);
   document.getElementById("regIdTextarea").value = result;
 }
 
 // Android and Amazon Fire OS
 function onNotification(e) {
-  alert("RECEIVED:" + JSON.stringify(e));
+  alert('RECEIVED:' + JSON.stringify(e));
 
   switch( e.event )
   {
-    case "registered":
+    case 'registered':
       if ( e.regid.length > 0 )
       {
         // Your GCM push server needs to know the regID before it can push to this device
-        alert("REGID:" + e.regid);
+        alert('REGID:' + e.regid);
         document.getElementById("regIdTextarea").value = e.regid;
 
         window.regid = e.regid;
@@ -104,13 +104,9 @@ function onNotification(e) {
       // e.payload.message
       // e.payload.msgcnt
       // e.payload.timeStamp
-      if ( e.foreground ){
-      }
-      else{  // otherwise we were launched because the user to xuched a notification in the notification tray.
-      }
     break;
 
-    case "error":
+    case 'error':
       // e.msg
     break;
   }

c9189e2dc3e0ae822656920448a59748112b241d
Ian Guerin <img236@nyu.edu>
2014-12-09 15:02:50 -0500

    hardcoding all functions

--- phonegapCode.js
+++ phonegapCode.js
@@ -1,14 +1,1 @@
-
-function sendToken(token, error) {
-  sendMessageToPlatform({token: token, error: error});
-  registerForPushNotification();
-}
-
-function fbLoginSuccess(userData) {
-  facebookConnectPlugin.getAccessToken(function(token) {
-      sendToken(token, "");
-  }, function(error) {
-      sendToken("", error);
-  });
-}
-
+// nothing here 
